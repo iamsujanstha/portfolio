14b1845925ec676ab90374770e52f3806d20171b
Sujan Shrestha <beingsujan.co@gmail.com>
2022-03-26 11:47:00 +0545

    new commit

--- personalBlog/settings.py
+++ personalBlog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-unq)i!sq3i=p04l!-g4g4xs&%d#r@k&d8=lr)xk89obpla&c^s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -130,7 +130,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
